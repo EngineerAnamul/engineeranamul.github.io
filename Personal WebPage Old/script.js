38cf98a32372567e0aa545d6984a1f7d3bc56042
Problem Solver <anamuli699@gmail.com>
2025-09-21 23:57:07 +0600

    Initial commit

---
+++ Personal WebPage Old/script.js
@@ -0,0 +1,29 @@
+// Smooth scroll for navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
+
+
+var typed = new Typed(".text", {
+    strings: ["Programming" , "Cybersecurity" , "Web Development"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
+const toTop = document.querySelector(".top");
+window.addEventListener("scroll",() =>{
+    if (window.pageYOffset > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
