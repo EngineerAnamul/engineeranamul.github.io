212ef5895f2388b0dc2c09aa06e7a6954b534461
EngineerAnamul <mdanamuli699@gmail.com>
2025-11-10 00:22:40 +0600

    code update

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-// Smooth scroll for navigation
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth"
+// naver scrooll color set
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  navbar.classList.toggle("scrolled", window.scrollY > 50);
+});
+
+  
+
+
+
+   // Navbar link click করলে auto close হবে (mobile view-তে)
+  document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
+    link.addEventListener('click', () => {
+      const navbarCollapse = document.querySelector('.navbar-collapse');
+      const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+        toggle: false
+      });
+      bsCollapse.hide();
     });
   });
-});
+
+
+
+// // Smooth scroll for navigation
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//   anchor.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     document.querySelector(this.getAttribute("href")).scrollIntoView({
+//       behavior: "smooth"
+//     });
+//   });
+// });
+
+
+
 
 
 var typed = new Typed(".text", {
